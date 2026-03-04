61426dcb1266aaf9a34228d1a5373f787ea3e07f
Rishab Karki <nullrish@gmail.com>
2026-03-04 15:24:35 +0545

    Initialized method signature for task repository

--- internal/repositories/task/repository.go
+++ internal/repositories/task/repository.go
@@ -1,1 +1,50 @@
 package task
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/google/uuid"
+	model "github.com/nullrish/task-manager-go/internal/models/task"
+)
+
+type TaskRepository interface {
+	CreateTask(ctx context.Context, task *model.TaskRequest) error
+	GetTaskByID(ctx context.Context, tID uuid.UUID) (*model.Task, error)
+	GetTasksByUserID(ctx context.Context, uID uuid.UUID) ([]model.Task, error)
+	GetTasks(ctx context.Context) ([]model.Task, error)
+	UpdateTask(ctx context.Context, task *model.TaskRequest) error
+	DeleteTask(ctx context.Context, tID uuid.UUID) error
+}
+
+type taskRepo struct {
+	db *sql.DB
+}
+
+func NewTaskRepository(db *sql.DB) TaskRepository {
+	return &taskRepo{db: db}
+}
+
+func (tr *taskRepo) CreateTask(ctx context.Context, task *model.TaskRequest) error {
+	return nil
+}
+
+func (tr *taskRepo) GetTaskByID(ctx context.Context, tID uuid.UUID) (*model.Task, error) {
+	return nil, nil
+}
+
+func (tr *taskRepo) GetTasksByUserID(ctx context.Context, uID uuid.UUID) ([]model.Task, error) {
+	return nil, nil
+}
+
+func (tr *taskRepo) GetTasks(ctx context.Context) ([]model.Task, error) {
+	return nil, nil
+}
+
+func (tr *taskRepo) UpdateTask(ctx context.Context, task *model.TaskRequest) error {
+	return nil
+}
+
+func (tr *taskRepo) DeleteTask(ctx context.Context, tID uuid.UUID) error {
+	return nil
+}
